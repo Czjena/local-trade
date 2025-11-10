82c178472e93b808e8503882d8f06c8a841e5755
Czjena <wieczorek.adrian12@gmail.com>
2025-11-10 03:46:39 +0100

    Revert "Merge branch 'main' of github.com:czjena/local-trade"
    This reverts commit 919d846f31eb90aeac97bff2486f7217a1093129, reversing
    changes made to 95f786b2023cfcfa8bf2a145ec3b7e62ad6fcee1.

--- services/main-api/src/main/java/io/github/czjena/local_trade/service/facade/NewAdvertisementFacade.java
+++ services/main-api/src/main/java/io/github/czjena/local_trade/service/facade/NewAdvertisementFacade.java
@@ -10,15 +10,9 @@ import io.github.czjena.local_trade.repository.UsersRepository;
 import io.github.czjena.local_trade.request.RequestAdvertisementDto;
 import io.github.czjena.local_trade.response.ResponseAdvertisementDto;
 import io.github.czjena.local_trade.response.SimpleAdvertisementResponseDto;
-<<<<<<<< HEAD:services/main-api/src/main/java/io/github/czjena/local_trade/service/facade/NewAdvertisementFacade.java
 import io.github.czjena.local_trade.service.infrastructure.AdvertisementService;
 import io.github.czjena.local_trade.service.infrastructure.NotificationEventPublisher;
 import io.github.czjena.local_trade.service.infrastructure.S3Service;
-========
-import io.github.czjena.local_trade.service.AdvertisementService;
-import io.github.czjena.local_trade.service.NotificationEventPublisher;
-import io.github.czjena.local_trade.service.S3Service;
->>>>>>>> cd431003e3b6545b01df5252ea913ca234526f9d:services/main-api/src/main/java/io/github/czjena/local_trade/facade/NewAdvertisementFacade.java
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -35,25 +29,18 @@ public class NewAdvertisementFacade {
     private final UsersRepository usersRepository;
     private final AdvertisementDtoMapper advertisementDtoMapper;
     private final AdvertisementRepository advertisementRepository;
-    private final NotificationEventPublisher notificationEventPublisher;
 
     public NewAdvertisementFacade(AdvertisementService advertisementService,
                                   S3Service s3Service,
                                   UsersRepository usersRepository,
                                   AdvertisementDtoMapper advertisementDtoMapper,
-<<<<<<<< HEAD:services/main-api/src/main/java/io/github/czjena/local_trade/service/facade/NewAdvertisementFacade.java
                                   AdvertisementRepository advertisementRepository
                                   ) {
-========
-                                  AdvertisementRepository advertisementRepository,
-                                  NotificationEventPublisher notificationEventPublisher) {
->>>>>>>> cd431003e3b6545b01df5252ea913ca234526f9d:services/main-api/src/main/java/io/github/czjena/local_trade/facade/NewAdvertisementFacade.java
         this.advertisementService = advertisementService;
         this.s3Service = s3Service;
         this.usersRepository = usersRepository;
         this.advertisementDtoMapper = advertisementDtoMapper;
         this.advertisementRepository = advertisementRepository;
-        this.notificationEventPublisher = notificationEventPublisher;
     }
 
     @Transactional
@@ -69,7 +56,6 @@ public class NewAdvertisementFacade {
                 advertisement.getImages().add(image);
             }
         }
-        notificationEventPublisher.publishAndCreateEvent(user, advertisement);
         return advertisementDtoMapper.toResponseAdvertisementDto(advertisement);
  }
 }
